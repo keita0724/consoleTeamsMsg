8d0a268630143449d85e339010c7f38cc9b9c097
keita0724 <knakagawakeita@ezweb.ne.jp>
2021-03-03 17:27:56 +0900

    first commit

---
+++ consoleTeams_0301/Graph/GraphHelper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimeZoneConverter;
+
+namespace consoleTeams_0301
+{
+    public interface GraphHelper
+    {
+        private static GraphServiceClient graphClient;
+        public static void Initialize(IAuthenticationProvider authProvider)
+        {
+            graphClient = new GraphServiceClient(authProvider);
+        }
+
+        public static async Task<ChatMessage> PostMessageAsync()
+        {
+            try
+            {
+                var chatMessage = new ChatMessage
+                {
+                    Body = new ItemBody
+                    {
+                        Content = "Hello world"
+                    }
+                };
+
+                return await graphClient.Chats["19:860caee54eaa4f2e822d38350c07b7fa@thread.v2"].Messages
+                    .Request()
+                    .AddAsync(chatMessage);
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IChatMessagesCollectionPage> GetMessageAsync()
+        {
+            try
+            {
+                var messages = await graphClient.Me.Chats["19:860caee54eaa4f2e822d38350c07b7fa@thread.v2"].Messages
+                    .Request()
+                    .GetAsync();
+
+                return messages;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ consoleTeams_0301/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace consoleTeams_0301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".NET Core Graph Tutorial\n");
+
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json...exiting");
+                return;
+            }
+
+            var appId = appConfig["appId"];
+            var scopesString = appConfig["scopes"];
+            var scopes = scopesString.Split(';');
+
+            // Initialize the auth provider with values from appsettings.json
+            var authProvider = new DeviceCodeAuthProvider(appId, scopes);
+
+            // Request a token to sign in the user
+            var accessToken = authProvider.GetAccessToken().Result;
+
+            Console.WriteLine($"Access token: {accessToken}\n");
+
+            // Initialize Graph client
+            GraphHelper.Initialize(authProvider);
+
+            var message = GraphHelper.PostMessageAsync().Result;
+            Console.WriteLine($"Welcome {message}!\n");
+
+
+            static IConfigurationRoot LoadAppSettings()
+            {
+                var appConfig = new ConfigurationBuilder()
+                    .AddUserSecrets<Program>()
+                    .Build();
+
+                // Check for required settings
+                if (string.IsNullOrEmpty(appConfig["appId"]) ||
+                    string.IsNullOrEmpty(appConfig["scopes"]))
+                {
+                    return null;
+                }
+
+                return appConfig;
+            }
+        }
+    }
+}
